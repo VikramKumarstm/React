836b8a7120243338fdba67a5bd65a7e9b888f5aa
VikramKumarstm <vikramclg0000@gmail.com>
2025-09-12 23:24:09 +0530

    add items components completed

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [todolist, setTodolist] = useState([])
+  
+  let saveToDoList = (event) => {
+    
+    let todoname= event.target.todoname.value;
+
+    if(!todolist.includes(todoname)) {
+
+      let finalTodolist = [...todolist, todoname]
+      setTodolist(finalTodolist)
+
+    } else {
+      alert("ToDo name allready exists.")
+    }
+
+    event.preventDefault();
+
+  }
+
+  let list = todolist.map((value, i) => {
+    return (
+
+      <ToDoListItems value={value} key={i}/>
+
+    )
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <h1>ToDo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type='text' name='todoname' /> <button>Save</button>
+      </form>
+
+      <div className="outerDiv">
+        <ul>
+          {list}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
 export default App
+
+function ToDoListItems({value}) {
+  return (
+    <li>{value}<span>&times;</span></li>
+  )
+}
