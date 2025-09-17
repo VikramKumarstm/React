913563d0d958ed74e6873d31eb05edca5e068166
VikramKumarstm <vikramclg0000@gmail.com>
2025-09-17 12:32:28 +0530

    complete controlled component

--- controlledComponent/src/App.jsx
+++ controlledComponent/src/App.jsx
@@ -4,31 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("name :",name)
+    console.log("password :", password)
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <div className='input-box'>
+          <label>User Name</label>
+          <input type="text" onChange={()=> setName(event.target.value)} value={name} />
+        </div>
+        <div className='input-box'>
+          <label>Password</label>
+          <input type="password" onChange={()=> setPassword(event.target.value)} value={password} />
+        </div>
+        <div className='input-box'>
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
