353faf44a7992700b09510c88ad23ee23ca68dd2
VikramKumarstm <vikramclg0000@gmail.com>
2025-09-18 18:25:36 +0530

    Start working on enquiry form using controlled component

--- enquiry-form/src/App.jsx
+++ enquiry-form/src/App.jsx
@@ -1,34 +1,35 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <form>
+        <div className="input-box">
+          <label>User Name</label>
+          <input type="text" name='name' />
+        </div>
+        <div className="input-box">
+          <label>Email</label>
+          <input type="email" name='email' />
+        </div>
+        <div className="input-box">
+          <label>Number</label>
+          <input type="tel" name='number' />
+        </div>
+        <div className="input-box">
+          <label>Message</label>
+          <textarea rows={6} name='message' />
+        </div>
+        <div className="input-box">
+          <button>Save</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
