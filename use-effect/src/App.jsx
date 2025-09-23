33ec091c3e3fb62c96a66147ccd93838034e94b6
VikramKumarstm <vikramclg0000@gmail.com>
2025-09-23 18:15:55 +0530

    complete all concept of useEffect

--- use-effect/src/App.jsx
+++ use-effect/src/App.jsx
@@ -1,10 +1,25 @@
+import {  } from 'react'
 import './App.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
 
+  let [counter, setCouter] = useState(1)
+
+  useEffect(() => {
+    console.log("Hello Vikram")
+  }, [counter])
+
+  const changeCounter = () => {
+    setCouter(counter++)
+  }
+
   return (
     <div className='app'>
-      
+      <h1>Use Effect Hook</h1>
+      {counter}
+      <button onClick={changeCounter}>Count</button>
     </div>
   )
 }
